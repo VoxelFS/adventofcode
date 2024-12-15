f8624464f629717b29098f5daac10edfdbcc9f15
VoxelFS <supicw@gmail.com>
2024-12-15 15:32:32 +1300

    Added day 5 part two solution

--- day5.py
+++ day5.py
@@ -24,6 +24,50 @@ def middle_sum(rules, lines):
     print(total)
 
 
+def correct_sum(rules, lines):
+    total = 0
+    incorrects = []
+    for i in range(len(lines)):
+        line_split = lines[i].split(",")
+        valid = False
+        for num in line_split:
+            before = rules[num]
+            for j in before:
+                try:
+                    index = line_split.index(j)
+                    if index > line_split.index(num):
+                        continue
+                    else:
+                        valid = True
+                        break
+                except ValueError:
+                    continue
+        if valid:
+            incorrects.append(line_split)
+    def find_incorrect(rules1, line):
+        for char in line:
+            before1 = rules1[char]
+            for k in before1:
+                try:
+                    index1 = line.index(k)
+                    index2 = line.index(char)
+                    if index1 > line.index(char):
+                        continue
+                    else:
+                        return True, index1, index2
+                except ValueError:
+                    continue
+        return False, -1, -1
+
+    for incorrect in incorrects:
+        cond = find_incorrect(rules, incorrect)
+        while cond[0]:
+            incorrect[cond[1]], incorrect[cond[2]] = incorrect[cond[2]], incorrect[cond[1]]
+            cond = find_incorrect(rules, incorrect)
+        total += int(incorrect[len(incorrect) // 2])
+    print(total)
+
+
 def main():
     arr = []
     with open("inputs/day5.txt", 'r') as file:
@@ -38,6 +82,7 @@ def main():
             rules_dict[split[0]].append(split[1])
 
         middle_sum(rules_dict, print_line)
+        correct_sum(rules_dict, print_line)
 
 
 if __name__ == "__main__":

65aeabd4308da45fb8de2e7c1b84a77fbe2cdf18
VoxelFS <oli444@aucklanduni.ac.nz>
2024-12-20 14:37:42 +1300

    Added day 8 part 1 solution

---
+++ day8.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+
+def anti_node_locations(antennas, max_x, max_y):
+    unique_locations = set()
+    for key, val in antennas.items():
+        for p in val:
+            for p2 in val:
+                if p != p2:
+                    dX = p[0] - p2[0]
+                    dY = p[1] - p2[1]
+                    anti_node1 = (p[0] + dX, p[1] + dY)
+                    if 0 <= anti_node1[0] <= max_x and 0 <= anti_node1[1] <= max_y:
+                        unique_locations.add(anti_node1)
+    return len(unique_locations)
+
+
+def main():
+    antenna_dict = defaultdict(list)
+    with open("inputs/day8.txt", 'r') as file:
+        arr = file.read().strip().split("\n")
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] != ".":
+                antenna_dict[arr[i][j]].append((j, i))
+    max_x = len(arr[0]) - 1
+    max_y = len(arr) - 1
+    print(anti_node_locations(antenna_dict, max_x, max_y))
+
+
+if __name__ == "__main__":
+    main()
